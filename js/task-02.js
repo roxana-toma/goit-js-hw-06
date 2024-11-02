14599bb4b063d9c1ad4fded0130786a894c6542c
roxana-toma <toma.iroxana@gmail.com>
2024-11-02 22:42:54 +0100

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Scrieți un script care pentru fiecare element al matricei ingredients:
+
+// 1. Va crea un element <li> separat. Asigurați-vă că folosiți metoda document.createElement().
+// 2. Adăugați numele ingredientului ca conținut text.
+// 3.Adăugați clasa item la element.
+
+const ingredientsList = document.getElementById("ingredients");
+
+const items = ingredients.map((ingredient) => {
+  const li = document.createElement ("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
+})
+
+// 4. Apoi va insera toate elementele <li> dintr-o singură operație în lista ul#ingredients.
+
+ingredientsList.append(...items);
